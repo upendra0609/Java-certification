6d5a7db85efe0716dd03930ce311a4b116a71514
Upendra <upendrasatna@gmail.com>
2023-11-18 20:39:05 +0530

    ocja

---
+++ Identifier/src/com/sikku/Main.java
@@ -0,0 +1,19 @@
+package com.sikku;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        a to z
+        A to Z
+        _ $
+        1. Identifier can not start with number
+        2. can start with _
+        3. can start with $
+        4. All predefined class name and interface name can be used as identifiers.(not recommended)
+
+         */
+
+        int num = 10_000_000;
+
+    }
+}
