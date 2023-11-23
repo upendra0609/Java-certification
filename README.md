bc4b789d364398dddea63be206bd8f146cef99d9
Upendra <upendrasatna@gmail.com>
2023-11-23 21:09:08 +0530

    ocja

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Java-certification
 OCJA
+
+
+Array---------------
+Every array in java is an object.
+
+/*
+    Two dimensional-->
+        int [][] a;
+        int a[][];
+        int[][]a;
+        int[] []a;
+        int[] a[];
+        int []a[];
+     */
+
+    /*
+    Two dimensional-->
+        int[][][] a;
+        int [][][]a;
+        int a[][][];
+        int[][] []a;
+        int[][] a[];
+        int[] [][]a;
+        int[] []a[];
+        int[] a[][];
+        int [][]a[];
+        int []a[][];
+     */
+
+
+    /*
+    int[] a,b;    a-1,b-1
+    int[] a[],b;  a-2,b-1
+    int[] []a,b;  a-2,b-2
+
+    int[] a,[]b;  //compiler time error(we can declare[] before 		    only first variable only)
+    int[] a,b[]; a-1, b-2
+
+    [] declaration before first variable is applicable for all     	variable(if any space, will be ignored by compiler)
+     */
+
+
+	int[] a = new int[-2];   //runTime error not compiler time error (compiler only check if you are providing int value or not)
---
+++ java-certification/src/com/sikku/array/Main.java
@@ -0,0 +1,62 @@
+package com.sikku.array;
+
+public class Main extends Thread {
+
+    /*
+    Two dimensional-->
+        int [][] a;
+        int a[][];
+        int[][]a;
+        int[] []a;
+        int[] a[];
+        int []a[];
+     */
+
+    /*
+    Two dimensional-->
+        int[][][] a;
+        int [][][]a;
+        int a[][][];
+        int[][] []a;
+        int[][] a[];
+        int[] [][]a;
+        int[] []a[];
+        int[] a[][];
+        int [][]a[];
+        int []a[][];
+     */
+
+
+    /*
+    int[] a,b;    a-1,b-1
+    int[] a[],b;  a-2,b-1
+    int[] []a,b;  a-2,b-2
+
+    int[] a,[]b;  //compiler time error(we can declare[] before only first variable only)
+    int[] a,b[]; a-1, b-2
+
+    [] declaration before first variable is applicable for all variable(if any space, will be ignored by compiler)
+     */
+
+    /*
+    Anonymous Array
+    new int[] {1,2,3}; //valid
+    new int[3] {1,2,3}; //Invalid
+    new int[4] {1,2,3}; //Invalid
+    new int[4]; //valid
+
+    in case of anonymous array we can not declare array size if elements are initialized
+     */
+    public static void main(String[] args) {
+        int[] a = new int[-2];  //runtime error
+        int[] b = new int[1212];
+        System.out.println(a.length);
+        m(new int[78]);
+
+    }
+
+    public static void m(int[] ar) {
+    }
+
+}
+
